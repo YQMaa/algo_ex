9ebf72bbb386354393f7c0c35b6846b14f7211a6
YQMaa <92177564+YQMaa@users.noreply.github.com>
2021-05-02 22:22:31 +0000

    Version 3.2

--- src/cerebrum/neural_networks/feed_forward_net.h
+++ src/cerebrum/neural_networks/feed_forward_net.h
@@ -21,4 +21,5 @@ struct FeedForwardNet {
   template <size_t batch_size, template<typename> class ErrorFunction>
   using ForwardComputation =
     _ForwardComputation<T, batch_size, ErrorFunction<T>,
-                      
+                        ErrorFunction<T>::transforms_last_layer,
+          
