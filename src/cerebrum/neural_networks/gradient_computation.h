2431fb7a6864dc4e92ff117ec02ebc5ced062624
YQMaa <92177564+YQMaa@users.noreply.github.com>
2021-05-13 10:39:34 +0000

    Reduce

--- src/cerebrum/neural_networks/gradient_computation.h
+++ src/cerebrum/neural_networks/gradient_computation.h
@@ -1,4 +1,7 @@
 // Copyright (C) 2015 Tudor Berariu <tudor.berariu@gmail.com>
 
 #ifndef GRADIENT_COMPUTATION_H
-#define GRADIENT_COMPUTATIO
+#define GRADIENT_COMPUTATION_H
+
+#include <array>
+#include "cerebru
