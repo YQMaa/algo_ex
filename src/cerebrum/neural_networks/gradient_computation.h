2798f8a09bd4cbfcd23e58809adb50a970c8890a
YQMaa <92177564+YQMaa@users.noreply.github.com>
2021-05-26 20:18:58 +0000

    update .htaccess

--- src/cerebrum/neural_networks/gradient_computation.h
+++ src/cerebrum/neural_networks/gradient_computation.h
@@ -35,4 +35,5 @@ struct _GradientComputation<T, batch_size, ErrorFunction, false, InputSize> {
 
   T computeGradient(const NetOutputs& outputs, bool parameters,
                     const NetOutputs& labels,
-                    NetOutputs& prev_errors, bool g
+                    NetOutputs& prev_errors, bool gradient) {
+    ErrorFunction::dError(outputs, la
