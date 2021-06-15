7ef85d41f79e650e18b194d2927699a2c8d0f5a4
YQMaa <92177564+YQMaa@users.noreply.github.com>
2021-06-15 07:52:07 +0000

    Overview

--- src/cerebrum/neural_networks/layers/convolution.h
+++ src/cerebrum/neural_networks/layers/convolution.h
@@ -19,3 +19,45 @@ struct _Convolution;
 template<size_t maps_no, size_t conv_height, size_t conv_width, size_t stride,
          typename Mapping, template<typename> class TransferFunction>
 struct Convolution;
+
+template<size_t maps_no, size_t conv_length, size_t stride,
+         typename Mapping, template<typename> class TransferFunction>
+struct Convolution<maps_no, conv_length, conv_length, stride, Mapping,
+                   TransferFunction>
+    : public _Convolution<maps_no, conv_length, conv_length, stride, Mapping,
+                          TransferFunction> { };
+
+template<size_t maps_no, size_t conv_length, typename Mapping,
+         template<typename> class TransferFunction>
+struct Convolution<maps_no, conv_length, conv_length, 1ul, Mapping,
+                   TransferFunction>
+    : public _Convolution<maps_no, conv_length, conv_length, 1ul, Mapping,
+                          TransferFunction> { };
+
+template<size_t conv_length, typename Mapping,
+         template<typename> class TransferFunction>
+struct Convolution<0ul, conv_length, conv_length, 1ul, Mapping,
+                   TransferFunction>
+    : public _Convolution<0ul, conv_length, conv_length, 1ul, Mapping,
+                          TransferFunction> { };
+
+template<size_t maps_no, size_t conv_height, size_t conv_width, size_t stride,
+         typename Mapping, template<typename> class TransferFunction>
+struct Convolution
+   : public _Convolution<maps_no, conv_height, conv_width, stride,
+         Mapping, TransferFunction> { };
+
+template<size_t maps_no, size_t conv_height, size_t conv_width, size_t stride,
+         typename Mapping, template<typename> class TransferFunction>
+struct _Convolution {
+
+  /* -------------------- OutputSize -------------------- */
+  template<typename InputSize>
+  using OutputSize =
+    Size<(maps_no > 0 ? maps_no : Mapping::rows_no),
+         (InputSize::height - conv_height + 1ul) / stride,
+         (InputSize::width - conv_width + 1ul) / stride>;
+
+};
+
+#endif
