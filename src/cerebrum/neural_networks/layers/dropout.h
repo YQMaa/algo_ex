56b8f1c66c458d5c8995325d97c9ec12754fad30
YQMaa <92177564+YQMaa@users.noreply.github.com>
2021-08-01 15:14:12 +0000

    prod

--- src/cerebrum/neural_networks/layers/dropout.h
+++ src/cerebrum/neural_networks/layers/dropout.h
@@ -178,4 +178,4 @@ struct Dropout {
         const OutputRow& error_row =
           *reinterpret_cast<const OutputRow*>(errors[n].data());
         InputRow& prev_error_row =
-          *rei
+          *reinterpret_cast<InputRow*>(prev_errors[n].da
