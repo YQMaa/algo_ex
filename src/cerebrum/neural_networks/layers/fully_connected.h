ad9139c8baeab623396a9def312c82cda1fe8c77
YQMaa <92177564+YQMaa@users.noreply.github.com>
2021-08-03 14:51:09 +0000

    Refactor error handling

--- src/cerebrum/neural_networks/layers/fully_connected.h
+++ src/cerebrum/neural_networks/layers/fully_connected.h
@@ -32,3 +32,53 @@ struct FullyConnected {
   }
 
   template<typename InputSize>
+  static constexpr size_t
+  parameters_no() {
+    return parameters_array_size<InputSize>();
+  }
+
+  template<typename T, typename InputSize>
+  using Parameters = std::array<T, (InputSize::length + 1) * length>;
+
+ private:
+
+  template<typename T, typename InputSize, size_t batch_size>
+  using _LinearInputs = std::array<T, InputSize::length * batch_size>;
+
+  template<typename T, typename InputSize, size_t batch_size>
+  using _LinearOutputs = std::array<T, length * batch_size>;
+
+  template<typename T, typename InputSize>
+  using _Biases = std::array<T, OutputSize<InputSize>::length>;
+
+  template<typename T, typename InputSize>
+  using _WeightsRow = std::array<T, InputSize::length>;
+
+  template<typename T, typename InputSize>
+  using _Weights =
+    std::array<_WeightsRow<T, InputSize>, OutputSize<InputSize>::length>;
+
+  template<typename T, typename InputSize>
+  using _LinearWeights = std::array<T, InputSize::length * length>;
+
+ public:
+
+  template<typename T, typename InputSize>
+  inline static void
+  init_parameters(Parameters<T, InputSize>& parameters) {
+    std::random_device rd { };
+    std::default_random_engine e {rd()};
+    std::normal_distribution<T> next_parameter(0.0, 0.1);
+
+    for (size_t i = 0; i < parameters_array_size<InputSize>(); i++)
+      parameters[i] = next_parameter(e);
+  }
+
+
+  /* -------------------- Inputs, Hidden, and Outputs -------------------- */
+ public:
+
+  template<typename T, typename InputSize>
+  using Input = std::array<T, InputSize::length>;
+
+  template<typename T, typename InputSize, size_t batch_size>
