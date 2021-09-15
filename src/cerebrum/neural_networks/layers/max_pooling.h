269539efb5facaf51121dbea3fb01cef2a13e628
YQMaa <92177564+YQMaa@users.noreply.github.com>
2021-09-15 21:24:24 +0000

    CAAERS-4970 - Implement a consistency check report  UI

--- src/cerebrum/neural_networks/layers/max_pooling.h
+++ src/cerebrum/neural_networks/layers/max_pooling.h
@@ -66,4 +66,4 @@ struct MaxPooling {
     std::array<_InputMap<T, InputSize>, InputSize::maps_no>;
 
   template<typename T, typename InputSize, size_t batch_size>
-  
+  using _Inputs = std::array<_InputMaps<T
