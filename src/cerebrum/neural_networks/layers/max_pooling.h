ce2d04b552ab0e30846c15ab0b648914f97e8eb0
YQMaa <92177564+YQMaa@users.noreply.github.com>
2021-11-03 07:43:26 +0000

    —

--- src/cerebrum/neural_networks/layers/max_pooling.h
+++ src/cerebrum/neural_networks/layers/max_pooling.h
@@ -138,4 +138,5 @@ struct MaxPooling {
       __Hidden& _hidden = *reinterpret_cast<__Hidden*>(&hidden);
 
       for (size_t n = 0; n < batch_size; n++) {
-        for (size_t m = 0; m < OutputSize<InputSize
+        for (size_t m = 0; m < OutputSize<InputSize>::maps_no; m++) {
+          const __InputMap
