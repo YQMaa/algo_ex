c30fd33e5a98b8033bc07b89f4ad1d413dfe347d
YQMaa <92177564+YQMaa@users.noreply.github.com>
2022-02-18 05:34:00 +0000

    Minor clarification.

--- src/cerebrum/neural_networks/layers/max_pooling.h
+++ src/cerebrum/neural_networks/layers/max_pooling.h
@@ -210,4 +210,6 @@ struct MaxPooling {
 
       __Inputs& _prev_errors = *reinterpret_cast<__Inputs*>(&prev_errors);
       const __Outputs& _errors = *reinterpret_cast<const __Outputs*>(&errors);
-      const __Hidden& _hidden = *reinterpret_cast<const __Hidden*>(&hidd
+      const __Hidden& _hidden = *reinterpret_cast<const __Hidden*>(&hidden);
+
+      for (size_
