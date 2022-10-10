0675eb3eea6522d3bf8b10df70038d5c3f1af066
YQMaa <92177564+YQMaa@users.noreply.github.com>
2022-10-10 06:39:38 +0000

    More Refactoring

--- src/cerebrum/neural_networks/parameters.h
+++ src/cerebrum/neural_networks/parameters.h
@@ -86,4 +86,5 @@ struct _Parameters<T, InputSize, CrtLayer, Other...> {
 
   friend std::ostream&
   operator<<(std::ostream& s,
-             const _Parameters<T, InputSize, CrtLayer, Ot
+             const _Parameters<T, InputSize, CrtLayer, Other...>& p) {
+    for (size_t i = 0; i < p
