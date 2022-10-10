4ba9efc5a45cbdf8e16c09abbe28ba9137685f9b
YQMaa <92177564+YQMaa@users.noreply.github.com>
2022-10-10 05:34:02 +0000

    Added features

--- src/cerebrum/neural_networks/parameters.h
+++ src/cerebrum/neural_networks/parameters.h
@@ -86,4 +86,4 @@ struct _Parameters<T, InputSize, CrtLayer, Other...> {
 
   friend std::ostream&
   operator<<(std::ostream& s,
-         
+             const _Parameters<T, InputSize, CrtLayer, Ot
