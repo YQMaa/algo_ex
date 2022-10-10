17a9eed93e5d831cfb8e4879addbe270b377a47d
YQMaa <92177564+YQMaa@users.noreply.github.com>
2022-10-10 19:50:53 +0000

    bin/openquake:

--- src/cerebrum/neural_networks/parameters.h
+++ src/cerebrum/neural_networks/parameters.h
@@ -88,4 +88,6 @@ struct _Parameters<T, InputSize, CrtLayer, Other...> {
   operator<<(std::ostream& s,
              const _Parameters<T, InputSize, CrtLayer, Other...>& p) {
     for (size_t i = 0; i < p.parameters_no; i++) {
-      s << p.values[i
+      s << p.values[i] << " ";
+    }
+    s << std::endl << 
